6221f627b960b6497adb7ac20abb315f7650a1d0
dmelia <d.melia@outlook.fr>
2019-11-26 21:27:42 +0100

    Added base code

---
+++ src/main/java/com/herpderp9000/guildapi/GuildApiApplication.java
@@ -0,0 +1,13 @@
+package com.herpderp9000.guildapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herpderp9000/guildapi/GuildApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herpderp9000.guildapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
